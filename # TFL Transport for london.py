9d052bad09852f9490589f08c40dbcf9706a4155
Observer <80362048+68756d616e@users.noreply.github.com>
2023-01-13 15:45:22 +0000

    Add files via upload

---
+++ # TFL Transport for london.py
@@ -0,0 +1,32 @@
+# TFL Transport for london
+# Fares from 1 March 2022
+# ...
+
+# Welcome message
+print("Welcome to TFL Price list")
+
+print() # Empty space
+
+# General question - Either seperate questions or 1 question with multiple choices
+while True:
+    general_question = input("""Please choose one of the following!
+- New bus and tram prices
+- Pay as you go caps
+- Travelcards
+- River Bus services
+- IFS Cloud Cable Car
+Please type here: """)
+
+# New bus and tram prices
+
+# Pay as you go caps
+# For Tube, DLR, London Overground, Elizabeth line and National Rail services
+# Adult rate daily and weekly caps increased by an average of 3.8%.
+
+# Travelcards
+# Adult Travelcard prices increased by an average of 3.8%.
+
+# River Bus services
+# Pay as you go fares increased on all Uber boats by Thames Clippers.
+
+# IFS Cloud Cable Car
