883843d24955bb9216d66784a04ef3210ef9cc95
Observer <80362048+68756d616e@users.noreply.github.com>
2023-01-19 16:52:34 +0000

    Update # TFL Transport for london.py

--- # TFL Transport for london.py
+++ # TFL Transport for london.py
@@ -11,11 +11,11 @@ print() # Empty space
 while True:
     general_question = input("""Please choose one of the following!
 A - New bus and tram prices
-B Pay as you go caps
-- Travelcards
-- River Bus services
-- IFS Cloud Cable Car
-Please type a,b,c or d here: """)
+B - Pay as you go caps
+C - Travelcards
+D - River Bus services
+E - IFS Cloud Cable Car
+Please type a,b,c,d or e here: """)
 
 # New bus and tram prices
     if general_question == 'a' or general_question == 'A':
@@ -24,17 +24,14 @@ Please type a,b,c or d here: """)
 - Monthly = £89.50	
 - Annual =  £932 """)
         print() # Empty space
-        print("""Additional information 
-- Pay as you go caps
-- For Tube, DLR, London Overground, Elizabeth line and National Rail services
-- Adult rate daily and weekly caps increased by an average of 3.8%.""")
-
+        
 # Pay as you go caps
     elif general_question == 'b' or general_question == 'B':
-        print("") # Empty space
         print("""Pay as you go caps
+
 - For Tube, DLR, London Overground, Elizabeth line and National Rail services
 - Adult rate daily and weekly caps increased by an average of 3.8%.
+
 Zone(s)	                    One Day Anytime	    One Day Off-peak	Monday to Sunday
 Zone 1  only	            £7.70	            £7.70	            £38.40
 Zone 1  and 2	            £7.70	            £7.70	            £38.40
@@ -42,6 +39,7 @@ Zone 1, 2 and 3	            £9.00	            £9.00	            £45.20
 Zone 1, 2, 3 and 4	        £11.00	            £11.00	            £55.20
 Zone 1, 2, 3, 4 and 5	    £13.10	            £13.10	            £65.70
 Zone 1, 2, 3, 4, 5 and 6	£14.10	            £14.10	            £70.30""")
+        print() # Empty space
 
 # Travelcards
 # Adult Travelcard prices increased by an average of 3.8%.
@@ -56,9 +54,19 @@ Zone 1, 2, 3 and 4	        £14.40	            £14.40	    £55.20	    £212.00	
 Zone 1, 2, 3, 4 and 5	    £20.30	            £14.40	    £65.70	    £252.30	    £2,628
 Zone 1, 2, 3, 4, 5 and 6	£20.30	            £14.40	    £70.30	    £270	    £2,812
 """)
-        print() # Open space
+        print() # Empty space
         
 # River Bus services
 # Pay as you go fares increased on all Uber boats by Thames Clippers.
+    elif general_question == 'd' or general_question == 'D':
+        print("""River Bus services
+River   Zones	    Adult	Child
+Central	            £7.70	£3.85
+East	            £4.80	£2.40
+West	            £4.80	£2.40
+Central and East	£8.70	£4.35
+Central and West	£8.70	£4.35
+West to East	    £13.50	£6.75        """)
+        print() Empty space
 
 # IFS Cloud Cable Car
